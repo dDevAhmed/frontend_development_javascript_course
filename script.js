9703c6d61d332e09341b0dabeb52f5ed926120a5
Ahmed <ahmedmahmud1994@hotmail.com>
2024-06-28 11:41:58 +0100

    Jascript Initialized

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert('hello world!');
+
+const primaryColor = '#00004d';
+// const secondaryColor = '#4d4d00';
+const secondaryColor = '#fff';
+
+document.querySelector('body').style.backgroundColor = primaryColor;
+document.querySelector('body').style.color = secondaryColor;
+document.getElementsByClassName('nav-links').style.color = secondaryColor;
+// document.getElementsByClassName('nav-links').style.textdecoration = none;
+
+
+/********************
+    index
+********************/ 
+// document.getElementById('myname').innerText = 'Ahmed Mahmud';
+// document.getElementById('name').style.textAlign = 'center';
+
+// document.write('<h1 style="color: orange;">Welcome to Javascript</h1>');
+
+
+/********************
+    class activity 1
+********************/ 
+// document.getElementById('activity-1').innerText = ('1');
